e5f15fbe401469d3c7d9646bf2a95744224439d4
Davi Henrique dos Santos Silva <davi.henrique.silva597@gmail.com>
2024-05-11 13:28:48 -0300

    add: porta.tsx

---
+++ model/porta.ts
@@ -0,0 +1,44 @@
+export default class PortaModel {
+  #numero: number
+  #temPresente: boolean
+  #selecionada:boolean
+  #aberta: boolean
+
+  constructor(numero: number, temPresente = false, selecionada: boolean, aberta = false) {
+    this.#numero = numero;
+    this.#temPresente = temPresente;
+    this.#selecionada = selecionada;
+    this.#aberta = aberta;
+  }
+
+  get numero() {
+    return this.#numero;
+  }
+
+  get temPresente() {
+    return this.#temPresente;
+  }
+
+  get selecionada() {
+    return this.#selecionada;
+  }
+
+  get aberta() {
+    return this.#aberta;
+  }
+
+  desselecionar() {
+    const selecionada = false;
+    return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta);
+  }
+
+  alteranarSelecao() {
+    const selecionada = !this.selecionada;
+    return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta);
+  }
+
+  abrir() {
+    const aberta = true;
+    return new PortaModel(this.numero, this.temPresente, this.selecionada, aberta);
+  }
+}
