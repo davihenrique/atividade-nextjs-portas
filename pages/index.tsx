105c34dd76ffd05b3c24c32b50d55e9b4f34aa0f
Davi Henrique dos Santos Silva <davi.henrique.silva597@gmail.com>
2024-05-28 07:31:09 -0300

    add: router

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
-export default function Home() {
+import styles from "../styles/Formulario.module.css";
+import Cartao from "../components/cartao";
+import Link from "next/link";
 
+export default function Formulario() {
   return (
-    <div style={{display: "flex"}}>
-      <h1>IFormulário de Início!!!</h1>
-    </div>
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao></Cartao>
+      </div>
 
+      <div>
+        <Cartao></Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/4/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
+    </div>
   );
 }
