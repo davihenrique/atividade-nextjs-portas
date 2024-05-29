b756e55abc24cf752fa8de6d4ee0d0744983378c
Davi Henrique dos Santos Silva <davi.henrique.silva597@gmail.com>
2024-05-29 06:17:19 -0300

    final atividde

--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -10,7 +10,19 @@ export default function jogo() {
   const router = useRouter();
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [valido, setValido] = useState(false);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [portas, setPortas] = useState([]);
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    const portas = +router?.query.portas;
+    const temPresente = +router?.query.temPresente;
+
+    const qtePortaValida = portas >= 3 && portas <= 100;
+    const temPresenteValido = temPresente >= 1 && temPresente <= portas;
+    setValido(qtePortaValida && temPresenteValido);
+  }, [portas, router?.query.portas, router?.query.temPresente]);
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
@@ -36,7 +48,9 @@ export default function jogo() {
 
   return (
     <div id={styles.jogo}>
-      <div className={styles.portas}>{rederinzarPortas()}</div>
+      <div className={styles.portas}>
+        {valido ? rederinzarPortas() : <h1>Valores Inválidos</h1>}
+      </div>
       <div className={styles.botoes}>
         <Link href="/">
           <button>Reiniciar Jogo</button>
